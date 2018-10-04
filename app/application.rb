a8e48acc9b98656fe4fc80abcef3ba194ec31933
Nicolas Marini <nmarini44@gmail.com>
2018-10-04 22:40:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.include?(item_name)
-        resp.write "#{item.name}\n #{item.price}"
+        resp.write "#{item.name} #{item.price}"
       else
         resp.status = 400
         resp.write "Item not found"
