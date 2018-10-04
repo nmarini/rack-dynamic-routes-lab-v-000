f3367e73ca7671e8890e5b9e535da8ab59439efa
Nicolas Marini <nmarini44@gmail.com>
2018-10-04 22:33:04 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -1,13 +1,9 @@
 class Item
   attr_accessor :name, :price
-  @@all = []
+
   def initialize(name,price)
     @name = name
     @price = price
     @@all << self
-  end
-
-  def self.find(name)
-    @@all.map{|item| item.name == name}
   end
 end
