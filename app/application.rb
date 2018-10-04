f8d905b8333841b307197e52a79cb3e4f80557bc
Nicolas Marini <nmarini44@gmail.com>
2018-10-04 22:14:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,15 @@ class Application
     item = req.param["q"]
 
     if req.path.match(/items/)
-
+      Items.all.map do |i| 
+        if i.name == item 
+          resp.write "#{i.name} #{i.price}\n" 
+        else 
+          resp.status = 400 
+          resp.write "Item not found" 
+        end 
+      else 
+        resp.status = 404 
+        resp.write "Route not found"
   end
 end
