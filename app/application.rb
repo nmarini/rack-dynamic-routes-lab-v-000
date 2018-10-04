0485ca8e93a7192a2cbb65fdd2f7cace8de327d7
Nicolas Marini <nmarini44@gmail.com>
2018-10-04 22:30:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,18 +3,20 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    item = req.params["q"]
+    item_search = req.params["q"]
 
     if req.path.match(/items/)
       # item_name = req.path("/item/").split.last
-      if item = Item.find()
-      Item.all.map do |i|
-        if i.name == item
-          resp.write "#{i.name} #{i.price}\n"
-        else
-          resp.status = 400
-          resp.write "Item not found"
-        end
+      if item = Item.find(item_search)
+        resp.write "#{item.name}\n #{item.price}"
+      else
+      # Item.all.map do |i|
+      #   if i.name == item
+      #     resp.write "#{i.name} #{i.price}\n"
+      #   else
+        resp.status = 400
+        resp.write "Item not found"
+      end
     else
       resp.status = 404
       resp.write "Route not found"
