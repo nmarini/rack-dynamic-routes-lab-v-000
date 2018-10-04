2fa8589e83324959f031e0b759c2281ca3260d2b
Nicolas Marini <nmarini44@gmail.com>
2018-10-04 22:20:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,18 +4,18 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     item = req.params["q"]
-binding.pry
-    # if req.path.match(/items/)
-    #   Items.all.map do |i|
-    #     if i.name == item
-    #       resp.write "#{i.name} #{i.price}\n"
-    #     else
-    #       resp.status = 400
-    #       resp.write "Item not found"
-    #     end
-    # else
-    #   resp.status = 404
-    #   resp.write "Route not found"
-    # end
+
+    if req.path.match(/items/)
+      Items.all.map do |i|
+        if i.name == item
+          resp.write "#{i.name} #{i.price}\n"
+        else
+          resp.status = 400
+          resp.write "Item not found"
+        end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
   end
 end
